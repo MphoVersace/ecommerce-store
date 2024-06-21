020d469942f01370d6d861b191c52481bfa10371
Zoedube <rofhiwazoe11@gmail.com>
2024-06-21 02:02:54 +0200

    new files

---
+++ src/constants/data.js
@@ -0,0 +1,92 @@
+// Code for product details
+import images from './images';
+
+const product = [
+  {
+    title: 'Apple Watch',
+    tags: 'Series 5 SE',
+    price: ' $ 529.99',
+    image: images.applewatch
+  },
+  {
+    title: 'Sony ZX330BT',
+    tags: 'Light Grey',
+    price: '$ 39.99',
+    image: images.headphones,
+  },
+  {
+    title: 'Iphone 11',
+    tags: 'Serious Black',
+    price: '$ 619.99',
+    image: images.iphone11,
+  },
+  {
+    title: 'Iphone 11',
+    tags: 'Subway Blue',
+    price: '$ 619.99',
+    image: images.iphone11mint,
+  },
+  {
+    title: 'Iphone 11',
+    tags: 'Product RED',
+    price: '$ 619.99',
+    image: images.iphone11red,
+  },
+  {
+    title: 'Iphone 13 Pro',
+    tags: 'Sierra Blue',
+    price: '$ 619.99',
+    image: images.iphone12,
+  },
+  {
+    title: 'Iphone 13 Pro',
+    tags: 'Silver',
+    price: '$ 619.99',
+    image: images.iphone13white,
+  },
+  {
+    title: 'Iphone 13 Pro',
+    tags: 'Graphite',
+    price: '$ 619.99',
+    image: images.iphone13black,
+  },
+  {
+    title: 'MacBook Air 13-inch',
+    tags: 'Space Grey',
+    price: '$ 619.99',
+    image: images.laptop,
+  },
+  {
+    title: 'MacBook Pro 16-inch',
+    tags: 'Silver',
+    price: '$ 619.99',
+    image: images.macbook,
+  },
+  {
+    title: 'MacBook Air 15-inch',
+    tags: 'Space Grey',
+    price: '$ 619.99',
+    image: images.sidelaptop,
+  },
+  {
+    title: 'Samsung Galaxy S21  Ultra',
+    tags: 'Phantom Black',
+    price: '$ 619.99',
+    image: images.samsungone,
+  },
+  {
+    title: 'Samsung Galaxy Note20',
+    tags: 'Mystic Green & Mystic Bronze',
+    price: '$ 619.99',
+    image: images.samsungpinkgreen,
+  },
+  {
+    title: 'Samsung Galaxy S21',
+    tags: 'Phantom Violet',
+    price: '$ 619.99',
+    image: images.samsungpurple,
+  },
+  
+];
+
+export default { product };
