8351dcad46ce3860f5725d2e03feab6227385d6d
Zoedube <rofhiwazoe11@gmail.com>
2024-06-21 15:26:48 +0200

    image bug

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -3,15 +3,15 @@ import './ProductItem.css';
 
 const ProductItem = ({ title, price, tags, image }) => (
   <div className="product-item">
-  <div className="product-item__image">
-    <img src={image} alt={title} />
+    <div className="product-item__image-container">
+      <img src={image} alt={title} className="product-item__image" />
+    </div>
     <div className="product-item__details">
       <h3 className="product-item__title">{title}</h3>
       <p className="product-item__tags">{tags}</p>
       <p className="product-item__price">{price}</p>
     </div>
   </div>
-</div>
 );
 
 export default ProductItem;
