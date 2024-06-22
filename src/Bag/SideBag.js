1e1d0b952df8644afda8241c34869c55f0181edb
lethabomichelle <lethabomichelle@gmail.com>
2024-06-23 00:22:17 +0200

    link bag, home and items pages. added add to bag icon

--- src/Bag/SideBag.js
+++ src/Bag/SideBag.js
@@ -1,38 +1,37 @@
 import React from "react";
 import { data } from "../constants";
 import { Link } from "react-router-dom";
+import './sidebag.css';
 
 export let myBag = [];
 let setMyBag = () => { };
 
-export default function Bag() {
+export default function SideBag() {
     const [cartItems, setCartItems] = React.useState(myBag);
     myBag = cartItems;
     setMyBag = setCartItems;
     const total = cartItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
     return (
-        <div className="cart">
-            <h2>Bag</h2>
+        <div className="cart" style={{ backgroundColor: '#EAEAEA' }}>
+            <h1 className="bag-title">Bag</h1>
             {cartItems.length === 0 ? (
                 <p>Your cart is empty.</p>
             ) : (
-                <ul>
+                <ul className="cart-list">
                     {cartItems.map((product) => (
-                        <li key={product.id}>
+                        <li key={product.id} className="cart-item">
                             <img src={product.image} style={{ maxWidth: '60px' }} alt={product.title} />
                         </li>
                     ))}
                 </ul>
             )}
-            <p>Total: ${total.toFixed(2)}</p>
+            {/* <p>Total: ${total.toFixed(2)}</p> */}
             <a href="/bag-details">View More</a>
         </div>
     );
 };
 
-
-
-export function addToBag(productId) {
+export function addToSideBag(productId) {
     const product = data.products[productId];
     const index = myBag.findIndex((item) => item.id === productId);
     if (index >= 0) {
