b61c2622565185c67ca1cc93bdecf8ff2c2ad81e
Hlogie01 <135062998+Hlogie01@users.noreply.github.com>
2024-06-26 14:13:22 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import Address from './components/address';
+import Address from './components/payment';
 
 function App() {
   return (
