795ae58ebdfd15fc93daf50018a524939d8d1593
Hlogie01 <135062998+Hlogie01@users.noreply.github.com>
2024-06-26 09:01:51 +0200

    update

--- src/components/address.jsx
+++ src/components/address.jsx
@@ -1,76 +1,76 @@
-import lock from "../assets/cardlock.svg";
 import { Link } from "react-router-dom";
+import lock from "../assets/cardlock.svg";
 import "../style2.css";
 
 function AddAdress() {
-    return (
-      <>
-        <div className="payment-container">
-          <div className="new-card-form">
-            <form>
-              <div className="form-group">
-                <div className="card">
-                  <label htmlFor="name">Shipping name </label>
-                  <input type="text" id="name" placeholder="John Maker" />
-                </div>
+  return (
+    <>
+      <div className="ccontainers">
+        <div className="bottom-ccontainer">
+          <form>
+            <div className="form-group">
+              <div className="card">
+                <label htmlFor="name">Shipping name </label>
+                <input type="text" id="name" placeholder="John Maker" />
               </div>
-              <div className="form-group">
-                <div className="card">
-                  <label htmlFor="name">Street name</label>
-                  <input
-                    type="text"
-                    id="name"
-                    placeholder="123 Plae Grond Street"
-                  />
-                </div>
+            </div>
+            <div className="form-group">
+              <div className="card">
+                <label htmlFor="name">Street name</label>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="123 Plae Grond Street"
+                />
               </div>
-  
-              <div className="form-group">
-                <div className="card">
-                  <label htmlFor="name">City</label>
-                  <input type="text" id="name" placeholder="Vermont" />
-                </div>
+            </div>
+
+            <div className="form-group">
+              <div className="card">
+                <label htmlFor="name">City</label>
+                <input type="text" id="name" placeholder="Vermont" />
               </div>
-  
-              <div className="form-group">
-                <div className="card">
-                  <label htmlFor="name">State/Province</label>
-                  <input type="text" id="name" placeholder="California" />
-                </div>
+            </div>
+
+            <div className="form-group">
+              <div className="card">
+                <label htmlFor="name">State/Province</label>
+                <input type="text" id="name" placeholder="California" />
               </div>
-  
-              <div className="form-group">
-                <div className="card">
-                  <label htmlFor="name">Country</label>
-                  <input type="text" id="name" placeholder="United States" />
-                </div>
+            </div>
+
+            <div className="form-group">
+              <div className="card">
+                <label htmlFor="name">Country</label>
+                <input type="text" id="name" placeholder="United States" />
               </div>
-              <br></br>
-  
-              <div className="default-payment-checkbox">
-                <input type="checkbox" id="myCheckbox" name="myCheckbox" />
-                <label htmlFor="myCheckbox">
-                  Save this as your default payment method
-                </label>
-              </div>
-  
-              <div className="submit-button">
-                <Link to="/AddPayment">
-                  <button className="bttn5">Add Payment Method</button>
-                </Link>
-              </div>
-  
-              <div className="footer">
-                <p>back</p>
-                <p id="la">
-                  <img src={lock} alt=""></img>Secure Connection
-                </p>
-              </div>
-            </form>
-          </div>
+            </div>
+            <br></br>
+
+            <div className="checkbox">
+              <input type="checkbox" id="myCheckbox" name="myCheckbox" />
+              <label htmlFor="myCheckbox">
+                Save this as your default payment method
+              </label>
+            </div>
+
+            <div className="btn5">
+              <Link to="/AddPayment">
+                <button className="bttn5">Add Address</button>
+              </Link>
+            </div>
+
+            <div className="la-line">
+              <p>back</p>
+              <p id="la">
+                <img src={lock} alt=""></img>Secure Connection
+              </p>
+            </div>
+          </form>
         </div>
-      </>
-    );
-  }
-  
-  export default AddAdress;
+      </div>
+    </>
+  );
+}
+
+export default AddAdress;
