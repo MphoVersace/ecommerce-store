9356563d1ca661c0d934195fef229c5470818353
Hlogie01 <135062998+Hlogie01@users.noreply.github.com>
2024-06-26 08:33:58 +0200

    update

--- src/components/payment.jsx
+++ src/components/payment.jsx
@@ -2,74 +2,57 @@ import cardIcon from "../assets/card-icon.svg";
 import lockIcon from "../assets/cardlock.svg";
 import "../style1.css";
 
-
-function PaymentForm() {
-    return (
-      <div className="payment-container">
-        <div className="card-selection">
-          <h1>Select a Card</h1>
-          <p>
-            <img src={cardIcon} alt="MasterCard" />
-            MasterCard ending in 4242
-          </p>
-          <p>
-            <img src={cardIcon} alt="VISA Debit" />
-            VISA Debit ending in 2894
-          </p>
+function AddPayment() {
+  return (
+    <>
+      <div className="containers">
+        <div className="top-container">
+          <h1>SELECT A CARD</h1>
+          <p><img src={cardIcon} alt="Card Icon"/>MasterCard ending in 4242</p>
+          <p><img src={cardIcon} alt="Card Icon"/>VISA Debit ending in 2894</p>
         </div>
-  
-        <div className="new-card-form">
+
+        <div className="bottom-container">
           <form>
-            <h1>Add a New Card</h1>
-  
+            <h1>ADD A NEW CARD</h1>
             <div className="form-group">
-              <div className="card-details">
-                <label htmlFor="cardholderName">Name</label>
-                <input type="text" id="cardholderName" placeholder="John Maker" className="card-input" />
+              <div className="card">
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" placeholder="John Maker" />
               </div>
             </div>
-  
             <div className="form-group">
-              <div className="card-details">
+              <div className="card">
                 <label htmlFor="cardNumber">Card Number</label>
-                <input type="text" id="cardNumber" placeholder="5126-5987-2214-7621" className="card-input" />
+                <input type="text" id="cardNumber" placeholder="5126-5987-2214-7621" />
               </div>
             </div>
-  
             <div className="form-group">
-              <div className="expiry-date">
+              <div className="Date">
                 <label htmlFor="expiryDate">Expiry Date</label>
                 <input type="text" id="expiryDate" placeholder="MM/YYYY" />
               </div>
-  
-              <div className="cvc-code">
+              <div className="Date">
                 <label htmlFor="cvc">CVC</label>
                 <input type="text" id="cvc" placeholder="123" />
               </div>
             </div>
-  
-            <div className="default-payment-checkbox">
-              <input type="checkbox" id="defaultPayment" name="defaultPayment" />
-              <label htmlFor="defaultPayment">Save this as your default payment method</label>
+            <div className="checkbox">
+              <input type="checkbox" id="myCheckbox" name="myCheckbox" />
+              <label htmlFor="myCheckbox">Save this as your default payment method</label>
             </div>
-  
-            <div className="submit-button">
-              <button type="button" className="submit-btn">
-                Add Payment Method
-              </button>
+            <div className="btn">
+              <button className="bttn">Add Payment Method</button>
             </div>
-  
-            <div className="footer">
-              <p className="back-link">Back</p>
-              <p className="secure-connection">
-                <img src={lockIcon} alt="Secure Connection" />
-                Secure Connection
-              </p>
+            <div className="last-line">
+              <p id="ba">Back</p>
+              <p id="la"><img src={lockIcon} alt="Lock Icon"/>Secure Connection</p>
             </div>
           </form>
         </div>
       </div>
-    );
-  }
-  
-  export default PaymentForm;
+    </>
+  );
+}
+
+export default AddPayment;
