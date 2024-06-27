3b2abdede9c81ab328ee4e1df09dd7173ee9171e
lethabomichelle <lethabomichelle@gmail.com>
2024-06-27 11:19:16 +0200

    changed the details display

--- src/container/ProductDetail/ProdcutDetail.jsx
+++ src/container/ProductDetail/ProdcutDetail.jsx
@@ -1,75 +1,52 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './ProductDetail.css';
-// import './sidebag.css';
-// import cart from '../assets/addcart.svg';
-// import stars from '../assets/Special.png';
-import { useLocation } from 'react-router-dom';
-import Sidebar from '../../components/MenuSidebar';
-import SideBag, { addToSideBag } from '../../Bag/SideBag';
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { data } from '../../constants';
 import StarIcon from '../../assets/StarIcon';
+import './ProductDetail.css'
+import SideBag from '../../Bag/SideBag';
+import Sidebar from '../../components/MenuSidebar';
 
-function ItemView() {
-  const location = useLocation();
-  const { item } = location.state;
-  const [clickedItem, setClickedItem] = useState(null);
-
-  const handleClick = () => {
-    setClickedItem(item);
-  };
+export default function ProductDetail() {
+  const { productId } = useParams();
+  console.log(productId)
+  const navigate = useNavigate();
+  const product = data.products[productId];
 
   return (
-    <> 
-    <div>
-      <Sidebar />
-      <Link to="/" className="back-link">
-        &lt;&nbsp;&nbsp;Back
-      </Link>
-      <div className="product-list">
-        {/* Render item details */}
-        <div className="product" key={item.id}>
-          {/* Render images */}
-          <div className="Container">
-            <div className="smallImg">
-              {/* Render small images */}
-              <img className="smallImage" src={item.imageUrl} alt={item.title} />
-              <img className="smallImage" src={item.imageUrl} alt={item.title} />
-              <img className="smallImage" src={item.imageUrl} alt={item.title} />
+    <>
+    <button onClick={() => navigate('/')} style={{display:'flex'}}>Back</button>
+    <div className="product-detail">
+      <Sidebar/>
+      <div >
+      <div >
+          <div>
+            <div style={{display:'flex'}}>
+              <div className="product-detail__image-container">
+                <img style={{width:'150px', height:'150px'}} src={product.image} alt={product.title} className="product-detail__image" />
+              </div>
+              <div className="product-detail__info">
+                <h2>{product.title}</h2>
+                <p>{product.tags}</p>
+                <div>
+                  <StarIcon />
+                </div>
+                <p>{product.price}</p>
+                <p>{product.shortDescription}</p>
+              </div>
             </div>
-            <div className="bigImg">
-              {/* Render big image */}
-              <img className="bigImage" src={item.imageUrl} alt={item.title} />
+              <p>{product.productDescription}</p>
             </div>
-          </div>
-          <div className="product-details">
-            <h1>{item.title}</h1>
-            <h3>{item.description}</h3>
-            <StarIcon />
-            <p>
-              {item.currency}
-              {item.price}
-            </p>
-            <p>{item.detailedDescription[0]}</p>
-          </div>
-        </div>
+          
+      
       </div>
-      <div className="button2" onClick={addToSideBag}>
-        {/* <img src={cart} alt="Logo" /> */}
-        <span>Add Bag</span>
+      <div>
+      <SideBag />
       </div>
-      <br />
-      <br />
-      <hr id="HR"></hr><br></br>
-      <div className="paragraph">
-        <h1>Description</h1>
-        <p>{item.detailedDescription[1]}</p>
-        {/* second part of detailed */}
       </div>
-      <SideBag/>
-      {/* <Menusidebar selectedItem={clickedItem} /> */}
       </div>
-    </>
+      </>
+  
   );
-}
+};
 
-export default ItemView;
+// export default ProductDetail;
